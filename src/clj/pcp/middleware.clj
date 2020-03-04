bf17ff7f315f66f95f64f3307a5ff63f58a0f56f
Alexander Oloo <alekcz@gmail.com>
2020-03-04 23:41:24 +0200

    fix mime-types

--- src/clj/pcp/middleware.clj
+++ src/clj/pcp/middleware.clj
@@ -4,33 +4,11 @@
     [cheshire.generate :as cheshire]
     [cognitect.transit :as transit]
     [clojure.tools.logging :as log]
-    [pcp.layout :refer [error-page]]
-    [ring.middleware.anti-forgery :refer [wrap-anti-forgery]]
     [pcp.middleware.formats :as formats]
     [muuntaja.middleware :refer [wrap-format wrap-params]]
     [pcp.config :refer [env]]
     [ring-ttl-session.core :refer [ttl-memory-store]]
-    [ring.middleware.defaults :refer [site-defaults wrap-defaults]])
-  
-           )
-
-(defn wrap-internal-error [handler]
-  (fn [req]
-    (try
-      (handler req)
-      (catch Throwable t
-        (log/error t (.getMessage t))
-        (error-page {:status 500
-                     :title "Something very bad has happened!"
-                     :message "We've dispatched a team of highly trained gnomes to take care of the problem."})))))
-
-(defn wrap-csrf [handler]
-  (wrap-anti-forgery
-    handler
-    {:error-response
-     (error-page
-       {:status 403
-        :title "Invalid anti-forgery token"})}))
+    [ring.middleware.defaults :refer [site-defaults wrap-defaults]]))
 
 
 (defn wrap-formats [handler]
@@ -45,5 +23,4 @@
       (wrap-defaults
         (-> site-defaults
             ;(assoc-in [:security :anti-forgery] false)
-            (assoc-in  [:session :store] (ttl-memory-store (* 60 30)))))
-      wrap-internal-error))
+            (assoc-in  [:session :store] (ttl-memory-store (* 60 30)))))))
--- src/clj/pcp/routes/processor.clj
+++ src/clj/pcp/routes/processor.clj
@@ -2,7 +2,7 @@
   (:require
    [clojure.java.io :as io]
    [pcp.middleware :as middleware]
-   [ring.util.response]
+   [ring.util.response :as resp]
    [ring.util.http-response :as response]
    [cheshire.core :as json]
    [sci.core :as sci]
@@ -30,40 +30,64 @@
     :anti-forgery-token (:anti-forgery-token request)})
 
 
-(defn append-extension [path]
-  (if true
-      (str "www/" path "/index.clj")
+(def config (atom {:localhost {:root "www/127.0.0.1"}}))
+(def mime-types { :json   { :content-type "application/json"  
+                            :encoder json/encode}
+                  :plain  { :content-type "text/plain"        
+                            :encoder str}
+                  :text   { :content-type "text/plain"        
+                            :encoder str}
+                  :html   { :content-type "text/html"         
+                            :encoder str}
+                  :csv    { :content-type "text/csv"        
+                            :encoder str}})
+
+(defn get-root [host]
+  (:root ((keyword host) @config)))
+
+(defn add-extension [path]
+  (if (str/ends-with? path "/") 
+      (str path "index.clj")
       path))
+
+(defn format-response [code body mime-type]
+  (let [status code
+        header (-> mime-types mime-type :content-type)
+        body ((-> mime-types mime-type :encoder) body)]
+   (-> (resp/response body)    
+       (resp/status status)
+       (resp/content-type header))))
 
 (defn read-source [path]
   (try
-    (str (slurp (append-extension path)))
-    (catch java.io.FileNotFoundException fnfe {:status 404 :body ""})))
+    (let [final-path (add-extension path)]
+      (println final-path)
+      (str (slurp final-path)))
+    (catch java.io.FileNotFoundException fnfe (format-response 404 nil :plain))))
 
 (defn load-source [pcp-params]
-  (let [host (:server-name pcp-params)
+  (let [host (get-root (:server-name pcp-params))
         path (:path pcp-params)]
     (read-source (str host path))))
 
 (defn run-source [source pcp-params]
   (let [opts  { ;:namespaces {'cheshire.core cheshire.core}
-                :bindings {'pcp (sci/new-var 'pcp pcp-params)}}]
-    (if (map? source)
-      source
-      (sci/eval-string source opts))))    
+                :bindings {'pcp (sci/new-var 'pcp pcp-params)
+                           'response (sci/new-var 'response format-response)}}]
+    (sci/eval-string source opts)))
 
 (defn process-request [request]
   (let [pcp-params (extract-request-parameters request)
         result (->  pcp-params
                     (load-source)
                     (run-source pcp-params))]
-    (if (map? result)           
-      { :status (or (:status result) 200)
-        :headers (merge {"Content-Type" "text/html"} (:headers result))
-        :body (if (contains? result :body) (:body result) result)}
-      { :status 200
-        :headers {"Content-Type" "text/html"}
-        :body result})))
+    (println result)
+    result))
+
+(defn request-handler [request]
+  (try 
+    (process-request request)
+    (catch Exception e (format-response 500 nil :plain))))
 
 (defn process-routes []
   [""
